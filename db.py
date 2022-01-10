5bd25ab31a18998de9096fa2d69c994d55e3b262
Sean Kernitsman <sean.kermail@gmail.com>
2022-01-09 23:31:13 -0500

    Fixed mongodb URI string

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import pymongo
 
 client = pymongo.MongoClient(
-    "mongodb+srv://vacation_tracker:vt5352@cluster0.skge2.mongodb.net/Main?retryWrites=true&w=majority&replicaSet=atlas-ceg8rh-shard-0",
+    "mongodb+srv://vacation_tracker:vt5352@cluster0.skge2.mongodb.net/VacationTracker?retryWrites=true&w=majority&replicaSet=atlas-ceg8rh-shard-0",
 )
 db = client.VacationTracker
