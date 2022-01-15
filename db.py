0d4f348b4cde70606d77f4bff6132425a8c02948
Sean Kernitsman <sean.kermail@gmail.com>
2022-01-14 23:00:34 -0500

    fixing Heroku

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import pymongo
 
 client = pymongo.MongoClient(
-    "mongodb+srv://vacation_tracker:vt5352@cluster0-shard-00-00.skge2.mongodb.net/VacationTracker?retryWrites=true&w=majority",
+    "mongodb+srv://vacation_tracker:vt5352@cluster0.skge2.mongodb.net/VacationTracker?retryWrites=true&w=majority",
 )
 db = client.VacationTracker
